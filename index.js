29eab1855374eb6565f8f9eea9ad1382400c5615
Sintia Januarini <sintia.merchant@gmail.com>
2023-03-27 00:57:37 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distanceInBlocks) {
+    return Math.abs(42-distanceInBlocks);
+}
+
+function distanceFromHqInFeet(distanceInBlocks) {
+    return distanceFromHqInBlocks(distanceInBlocks)*264;
+
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start-destination)*264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination)
+    if (distance>2500) {
+        return 'cannot travel that far';
+    }
+    else if(distance<400) {
+        return 0;
+    }
+    else if (distance>2000) {
+        return 25;
+    }
+
+    else if (400 < distance <=2000) {
+            return (distance-400)* 0.02;
+        }
+
+    
+}
